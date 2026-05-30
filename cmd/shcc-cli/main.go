25c33d45ef2f1c9d4cb09d80fd41cd9000ab2855
Dimitri ONGOUA <dimitri.ongoua@aninf.ga>
2026-05-30 14:36:36 +0100

    Build: Bump version to 1.5.0

--- cmd/shcc-cli/main.go
+++ cmd/shcc-cli/main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/mendoc/shcc/internal/system"
 )
 
-const version = "1.4.0"
+const version = "1.5.0"
 
 func main() {
 	// 1. Chargement du .env PRIORITAIRE depuis le répertoire de l'exécutable
